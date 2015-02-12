e723d6513f8f0a6c1555d6b5d1990a106bbbb9ef
tick <tick@tick-ubuntu.(none)>
2015-02-12 15:06:15 +0800

    change env variables saved base address to 15M of first flash

--- include/configs/zynq_common.h
+++ include/configs/zynq_common.h
@@ -196,7 +196,7 @@
 
 # define CONFIG_ENV_SECT_SIZE		CONFIG_ENV_SIZE
 # ifndef CONFIG_ENV_OFFSET
-#  define CONFIG_ENV_OFFSET		0xE0000
+#  define CONFIG_ENV_OFFSET		0xF00000
 # endif
 # define CONFIG_CMD_SAVEENV	/* Command to save ENV to Flash */
 #endif
